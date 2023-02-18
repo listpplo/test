5168ff277e6ee3a095d37d650b6e5741d43b3985
Swapnil Diwakar <83010366+listpplo@users.noreply.github.com>
2023-02-18 07:28:14 +0530

    This program gives the NSE data

--- test.py
+++ test.py
@@ -1,17 +1,42 @@
-# Setting the random seed 
-tf.random.set_seed(42)
+from nsetools import Nse 
+import datetime
+import os
+import time 
 
-#1. creating the model using TensorFlow
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(1)   # Dense function gives the number of neurons in the layer
+# Definf the nse object 
+nse = Nse()
+#getting the corret date and time
 
-])
 
-#2. compiling the model
-model.compile(loss =tf.keras.losses.mae,    # mae  --> Mean absolute error
-              optimizer = tf.keras.optimizers.SGD(),
-              metrics = ["mae"])
+time_stamp = datetime.datetime.now()
 
-#3. Fit the model
+# creating and reading the file
 
-model.fit(tf.expand_dims(X,axis = -1),Y, epochs = 10)
+#name1 = "file date -- "
+#name = name1 + str(time_stamp)
+lst_file = open("list.txt","r")
+data_file = open("data.txt", "a+")
+
+data_file.write("************************************************************************\n")
+data_file.write("Time : "+str(datetime.datetime.now())+"\n")
+data_file.write("________________________________________________________________________\n")
+#print(nse.get_quote("INFY"))
+symbols = lst_file.readlines()
+print(symbols)
+print(time.time())
+for symbol in symbols:
+  sym = symbol.split("\n")
+  #print(sym)
+  data = nse.get_quote(sym[0])
+  data_file.write(symbol)
+  data_file.write("Quantity selled : "+ str(data["totalSellQuantity"])+"\n")
+  data_file.write("Total Quantity Buyed : " +str(data["totalBuyQuantity"])+"\n")
+  data_file.write("Closing Price : "+str(data["closePrice"])+"\n")
+  data_file.write("Day Low : "+str(data["dayLow"])+"\n")
+  data_file.write("Day High : "+str(data["dayHigh"])+"\n")
+  data_file.write("\n")
+  data_file.write("\n")
+
+data_file.write("************************************************************************")
+#os.system("cat data.txt")
+data_file.close()
